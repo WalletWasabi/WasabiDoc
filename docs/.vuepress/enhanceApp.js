76800951738f98bf38dbcea7e0df7ac0fc81f918
Dennis Reimann <mail@dennisreimann.de>
2019-08-09 19:34:04 +0200

    Fix production build warnings
    Signed-off-by: Dennis Reimann <mail@dennisreimann.de>

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,7 +1,7 @@
 // https://v1.vuepress.vuejs.org/guide/basic-config.html#app-level-enhancements
 const openDetails = hash => {
   const el = document.getElementById(hash.substr(1))
-  console.log(el)
+
   if (el && el.tagName.toLowerCase() === 'details') {
     el.setAttribute('open', true)
     const { offsetTop } = el
@@ -9,16 +9,18 @@ const openDetails = hash => {
   }
 }
 
-export default ({ router }) => {
-  // initial page rendering
-  document.addEventListener('DOMContentLoaded', () => {
-    window.setTimeout(() => openDetails(location.hash), 500)
-  })
+export default () => {
+  if (typeof process === 'undefined' || process.env.VUE_ENV !== 'server') {
+    // initial page rendering
+    document.addEventListener('DOMContentLoaded', () => {
+      window.setTimeout(() => openDetails(location.hash), 500)
+    })
 
-  // sidebar link clicks
-  document.addEventListener('click', e => {
-    if (e.target.matches('.sidebar-link')) {
-      openDetails(location.hash)
-    }
-  })
+    // sidebar link clicks
+    document.addEventListener('click', e => {
+      if (e.target.matches('.sidebar-link')) {
+        openDetails(location.hash)
+      }
+    })
+  }
 }
