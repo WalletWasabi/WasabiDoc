704f6476521c80e359bd7aad66a38f6509dac998
Max Hillebrand <30683012+MaxHillebrand@users.noreply.github.com>
2019-08-20 18:35:16 +0200

    Add TransactionSurveillanceCompanies to sidebar

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -58,7 +58,8 @@ module.exports = {
           children: [
             "/why-wasabi/WhyPrivacyImportant.md",
             "/why-wasabi/BitcoinPrivacy.md",
-            "/why-wasabi/10Commandments.md"
+            "/why-wasabi/10Commandments.md",
+            "/why-wasabi/TransactionSurveillanceCompanies.md"
           ]
         }
       ],
