5fd9196bf231171bb9bc9348d104c87e9b59dfb4
Riccardo Masutti <46527252+RiccardoMasutti@users.noreply.github.com>
2020-09-21 16:40:22 +0200

    revert bitcoincore variable commit
    Commited by mistake. I need to create a PR for review

--- docs/.vuepress/variables.js
+++ docs/.vuepress/variables.js
@@ -2,8 +2,7 @@
 const variables = {
   currentVersion: '1.1.12',
   dotnetVersion: '3.1',
-  zksnacksPublicKeyFingerprint: '6FB3 872B 5D42 292F 5992 0797 8563 4832 8949 861E',
-  bitcoinCoreVersion: '0.20.1'
+  zksnacksPublicKeyFingerprint: '6FB3 872B 5D42 292F 5992 0797 8563 4832 8949 861E'
 }
 
 module.exports = source =>
