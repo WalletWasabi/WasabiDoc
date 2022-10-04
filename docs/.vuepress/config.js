b77109eb8a7eb06f8b9847c0672c49f2d4c7f7b8
MarnixCroes <93143998+MarnixCroes@users.noreply.github.com>
2022-10-04 12:25:08 +0200

    rename ColdWasabi to HardwareWallet

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -180,7 +180,7 @@ module.exports = {
             "/using-wasabi/Receive.md",
             "/using-wasabi/CoinJoin.md",
             "/using-wasabi/Send.md",
-            "/using-wasabi/ColdWasabi.md",
+            "/using-wasabi/HardwareWallet.md",
             "/using-wasabi/BitcoinFullNode.md"
           ]
         },
