a8f4f03466f8b65e9120e6550538ba52982189bc
Lucas Ontivero <lontivero@users.noreply.github.com>
2025-11-21 11:20:23 +0000

    deploy

---
+++ assets/DownloadMac-B9PW9y9k.js
@@ -0,0 +1,1 @@
+const o="/DownloadDeb.png",n="/DownloadTar.png",s="/DownloadWindows.png",a="/DownloadMac.png";export{o as _,n as a,s as b,a as c};
--- assets/DownloadMac-DUSPDgNE.js
+++
@@ -1,1 +0,0 @@
-const o="/DownloadDeb.png",n="/DownloadTar.png",s="/DownloadWindows.png",a="/InstallWindowsSignature.png",t="/DownloadMac.png";export{o as _,n as a,s as b,a as c,t as d};
